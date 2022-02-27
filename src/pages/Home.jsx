1277299e28fbac1413644858835d75032ba16265
Tomás Salina <salinatomass53@gmail.com>
2022-02-27 11:34:09 -0300

    getData

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react';
 import '../styles/Home.css';
 import Header from '../components/Header';
 import CurrentWeather from '../components/CurrentWeather';
 
 const Home = () => {
+  const getData = async () => {
+    const response = await fetch(
+      'https://www.metaweather.com/api/location/2487956/'
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <main className="hero">
       <Header />
